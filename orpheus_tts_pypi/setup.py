31d88daea4e487639c04544295cd10f216e473cd
Radovan Zivkovic <pivonroll@gmail.com>
2025-12-27 15:09:37 +0100

    Update url in setup.py

--- orpheus_tts_pypi/setup.py
+++ orpheus_tts_pypi/setup.py
@@ -11,7 +11,7 @@ setup(
     description="Orpheus Text-to-Speech System",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/canopyai/orpheus-tts",
+    url="https://github.com/pivonroll/Orpheus-TTS-async",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
